70ec07b203ec6dbd1b04b8f5426e761516105543
Ariel Gallardo <ariel.gallardo96@gmail.com>
2022-06-28 01:16:59 -0300

    movies# fix pathname movies reload

--- src/components/Elements/SearchBox.jsx
+++ src/components/Elements/SearchBox.jsx
@@ -24,23 +24,29 @@ const SearchBox = () => {
     const searchLocal = (e) => setQuery(e.target.value)
 
     useEffect(()=>{
-        if(searchEngine !== ''){
-            loadQuery()
+        if(pathname === '/movie/list'){
+            if(searchEngine !== ''){
+                loadQuery()
+            }else{
+                searchAll()
+            }
         }else{
-            searchAll()
+            setQuery('')
         }
-    },[searchEngine])
+
+    },[searchEngine, pathname])
 
 
     if(pathname === '/movie/list')
-    return <div className="nav-item d-flex flex-column justify-content-center">
-        <div className="d-flex h-25">
-            <input onChange={searchLocal} className="form-control me-1 searchBox" placeholder="Movie name..."/>
-            <i className='searchButton position-relative'>
-                <img className='position-absolute' alt='Search' src='/assets/images/searchIcon.svg'/>
-            </i>
+        return <div className="nav-item d-flex flex-column justify-content-center">
+            <div className="d-flex h-25">
+                <input onChange={searchLocal} className="form-control me-1 searchBox" placeholder="Movie name..."/>
+                <i className='searchButton position-relative'>
+                    <img className='position-absolute' alt='Search' src='/assets/images/searchIcon.svg'/>
+                </i>
+            </div>
         </div>
-    </div>
+
 }
 
 export default SearchBox
